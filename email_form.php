b53088506f32fecc1ca4effe9be40de0df15d9b8
gopalansh <gopalansh@yahoo.com>
2013-10-27 00:08:13 -0400

    Update email_form.php

--- email_form.php
+++ email_form.php
@@ -1,75 +1,67 @@
 <?php
-if(isset($_POST['email'])) {
-     
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "contact@agilitysolutions.us";
-    $email_subject = "Agility Work Request Form";
-     
-     
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
-     
-    // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['comments'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
-     
-    $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $comments = $_POST['comments']; // required
-     
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
-    $string_exp = "/^[A-Za-z .'-]+$/";
-  if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
-    $email_message = "Form details below.\n\n";
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-     
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
-     
-     
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-<!-- include your own success html here -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
-<?php
+
+/* Settings
+---------------------*/
+include('settings.php');
+
+/* Get Data
+---------------------*/
+$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$parts = parse_url($url);
+parse_str($parts['query'], $query);
+
+foreach ($query as $name => $value) {
+        $content .= ucfirst($name) . ': ' . $value . '<br />';
 }
+
+
+/* Form Functions
+---------------------*/
+$subject = ucwords($site_title) . ' ' . ucwords($form_type);
+
+$body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html><head></head><body bgcolor="#FFFFFF">
+
+
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <base target="_blank">
+
+
+
+                <div class="background" style="width: 100%;background: 100%;margin: 0;background-color: #f4f4f4;padding-bottom: 40px;">
+                        <div class="logo no-margin" style="background-color: ' . $headerbar_color . ' ;height: 44px;width: 100%;margin-bottom: 0;overflow: hidden;">
+                                        <p style="color: white;line-height: 1.4em;font-family:Arial;font-size: 15px; font-weight:800; margin-left:20px; margin-top:10px;">' . $site_title . ' - '. ucwords($form_type) . '</p>
+                                </div><!--end .logo-->
+                        <div class="container" style="margin: 0 auto;padding-bottom: 20px;width: 95%;max-width: 680px;background-color: #fff;border: 1px solid #dfdfdf;border-top: none;">
+
+                                <div class="header" style="padding-top:20px; margin: 0 20px 20px;overflow: hidden;">
+                                        <h1 mc:edit="header" style="font-family:Arial;font-size: 20px;line-height: 1.2em;margin: 0 0 5px;color: #222;">Hi ' . $webmaster_name . ',</h1>
+                                        <p mc:edit="main" class="intension" style="color: #555;line-height: 1.4em;font-family:Arial;font-size: 15px;margin: 0;"> We received ' . $form_type . ' from your website.</b></p><br />
+                                        <p mc:edit="main" class="intension" style="color: #555;line-height: 1.4em;font-family:Arial;font-size: 15px;margin: 0;">'. $content .'</b></p>
+                                        <br />
+                                        <p mc:edit="main" class="intension" style="color: #555;line-height: 1.4em;font-family:Arial;font-size: 15px;margin: 0;"><b>Email sent by yoongfook CMS
+
+                                </div><!--end .header-->
+
+
+                        </div><!--end .container-->
+
+                <!-- end .background-->
+
+</body></html>';
+//echo $body;
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+$headers .= 'From: ' . $site_title . ' <no-reply@email.com>' . "\r\n";
+
+if(mail($email_to, $subject, $body, $headers)) {
+        if(isset($success_redirect)) {
+                header("Location: $success_redirect");
+        } else {
+                echo 'success';
+        }
+}
+
 ?>
